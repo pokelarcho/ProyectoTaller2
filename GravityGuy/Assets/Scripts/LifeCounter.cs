ac05b027fa352403f77bf6fb1407845db753d544
pokelarcho <73141433+pokelarcho@users.noreply.github.com>
2020-11-15 22:33:07 -0500

    semana 11

---
+++ GravityGuy/Assets/Scripts/LifeCounter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LifeCounter : MonoBehaviour
+{
+
+     public TextMeshProUGUI Vidas;
+
+    private GameObject PlayerBoy;
+    private Death death;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        PlayerBoy = GameObject.Find("PlayerBoy");
+        death = PlayerBoy.GetComponent<Death>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vidas.text = death.lives.ToString("0");
+        /*if (!Vidas.text.Equals(death.lives.ToString()))
+            StartCoroutine(Pulse());*/
+
+            
+        
+    }
+
+    private IEnumerator Pulse() {
+
+        for (float i = 1f; i <= 1.2f; i += 0.05f)
+        {
+            Vidas.rectTransform.localScale = new Vector3(i, i, i);
+            yield return new WaitForEndOfFrame();
+        }
+        Vidas.rectTransform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+
+        Vidas.text = death.lives.ToString("0");
+
+        for (float i = 1.2f; i <= 1f; i += 0.05f)
+        {
+            Vidas.rectTransform.localScale = new Vector3(i, i, i);
+            yield return new WaitForEndOfFrame();
+        }
+        Vidas.rectTransform.localScale = new Vector3(0.5f, 0.5f,0.5f);
+    }
+}
--- GravityGuy/Assets/Scripts/Pause.cs
+++ GravityGuy/Assets/Scripts/Pause.cs
@@ -8,6 +8,8 @@ public class Pause : MonoBehaviour
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
     public GameObject LivesUI;
+    public GameObject x;
+    public GameObject Lives;
 
     // Update is called once per frame
     void Update()
@@ -25,14 +27,14 @@ public class Pause : MonoBehaviour
 
     void Resume(){
        pauseMenuUI.SetActive(false);
-        LivesUI.SetActive(true);
+       //LivesUI.SetActive(true);
         Time.timeScale = 1f;
       GameIsPaused = false;
     }
 
     void Pausa(){
       pauseMenuUI.SetActive(true);
-       LivesUI.SetActive(false);
+       //LivesUI.SetActive(false);
         Time.timeScale = 0f;
       GameIsPaused = true;
     }
