53e2554a87ec1294e3264b685fab30f5b9c853a5
pokelarcho <73141433+pokelarcho@users.noreply.github.com>
2020-11-24 15:40:58 -0500

    s13

---
+++ GravityGuy/Assets/Scripts/Framerater.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Framerater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 1;
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
